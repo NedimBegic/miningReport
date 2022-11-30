27968da51572292c0dac92b54d91f2c889944734
Nedim Begic <nedim.begic93@gmail.com>
2022-12-01 00:24:56 +0100

    finish notes

---
+++ js/notes.js
@@ -0,0 +1,24 @@
+// button
+let dodajNapomenu = document.getElementById('dodajNapomenu');
+// article for listing
+let listaNapomena = document.querySelector('.listaNapomena');
+// array for notes in memory
+let napomenaArr = JSON.parse(localStorage.getItem('napomenaArr')) || [];
+// listing all notes
+for(let i = 0;i<napomenaArr.length;i++) {
+    listaNapomena.innerHTML += `
+    <li class="note">${napomenaArr[i]}</li>
+
+    `
+}
+
+dodajNapomenu.addEventListener('click', () => {
+    let napomena = document.getElementById('napomena').value;
+    napomenaArr.push(napomena);
+    // putting notes in memory
+    localStorage.setItem('napomenaArr', JSON.stringify(napomenaArr));
+    listaNapomena.innerHTML += `
+        <li class="note">${napomenaArr[napomenaArr.length-1]}</li>
+    `
+
+})
